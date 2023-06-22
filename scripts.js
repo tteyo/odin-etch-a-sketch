3e605c1a9f1ec75aceabb2f790d1b06301160e32
Mateo <89845424+tteyo@users.noreply.github.com>
2023-06-23 00:33:33 +0800

    Add basic drawing functionality on 400x400 grid

--- scripts.js
+++ scripts.js
@@ -1,1 +1,10 @@
-document.querySelector('.container').innerText = "Hello World!"
+const drawingPad = document.querySelector('.container');
+
+for(let i = 0; i < 256; i++){
+    const gridBox = document.createElement('div');
+    gridBox.classList.add("gridBox");
+    gridBox.addEventListener('mouseover', () => {
+        gridBox.classList.add("colored");
+    });
+    drawingPad.appendChild(gridBox);
+}
