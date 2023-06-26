8331d4b02d9bdb97f8a51841654904b237a7815e
Mateo <89845424+tteyo@users.noreply.github.com>
2023-06-26 19:49:03 +0800

    Add rainbow trail functionality, redraw function as one

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 const drawingPad = document.querySelector('.container');
-const gridChanger = document.querySelector('.grid')
+const gridChanger = document.querySelector('.grid');
+const colorChanger = document.querySelector('.randomize');
+
+function randomColor(){
+    return `rgb(${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)},${Math.floor(Math.random()*255)})`;
+}
 
 function redraw(gridNumber){
     while(drawingPad.firstChild){
@@ -15,16 +20,25 @@ function redraw(gridNumber){
         gridBox.style.width = `${pxSize}px`;
         gridBox.style.height = `${pxSize}px`;
         gridBox.style.border = "1px solid grey";
+
+        gridBox.addEventListener('mouseover', () => {
+            if(gridBox.classList.contains("rainbow")){
+                gridBox.style.backgroundColor = randomColor();
+            }else{
+                gridBox.style.backgroundColor = "black";
+            }
+        });
+
         drawingPad.appendChild(gridBox);
     }
 }
 
 gridChanger.addEventListener('click', () => {
     redraw(prompt("Number of Squares per side? e.g 16 in a 16x16"));
-    
+});
+
+colorChanger.addEventListener('click', () => {
     drawingPad.childNodes.forEach(box => {
-        box.addEventListener('mouseover', () => {
-            box.classList.add("colored")
-        });
+        box.classList.toggle("rainbow");
     });
 });
