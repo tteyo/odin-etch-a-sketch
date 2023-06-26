bd48ca005968978fcba7c7284c18ed8415152c7e
Mateo <89845424+tteyo@users.noreply.github.com>
2023-06-26 19:02:33 +0800

    Refactor code to allow for modifiable grid

--- scripts.js
+++ scripts.js
@@ -1,10 +1,32 @@
 const drawingPad = document.querySelector('.container');
+const gridChanger = document.querySelector('.grid')
 
-for(let i = 0; i < 256; i++){
-    const gridBox = document.createElement('div');
-    gridBox.classList.add("gridBox");
-    gridBox.addEventListener('mouseover', () => {
-        gridBox.classList.add("colored");
-    });
-    drawingPad.appendChild(gridBox);
+function clear(){
+    while(drawingPad.firstChild){
+        drawingPad.removeChild(drawingPad.firstChild);
+    }
 }
+
+function resize(gridNumber){
+    if(!Number(gridNumber)) return;
+    let number = Number(gridNumber);
+    let pxSize = 400/number - 2;
+    
+    for(let i = 0; i < number*number; i++){
+        const gridBox = document.createElement('div');
+        gridBox.style.width = `${pxSize}px`;
+        gridBox.style.height = `${pxSize}px`;
+        gridBox.style.border = "1px solid grey";
+
+        gridBox.addEventListener('mouseover', () => {
+            gridBox.classList.add("colored");
+        });
+
+        drawingPad.appendChild(gridBox);
+    }
+}
+
+gridChanger.addEventListener('click', () => {
+    clear();
+    resize(prompt("Number of Squares per side? e.g 16 in a 16x16"));
+});
