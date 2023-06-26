b7bb179e2bd589607386a7cf0eed91d776f6f598
Mateo <89845424+tteyo@users.noreply.github.com>
2023-06-26 19:18:23 +0800

    Separate event Listener from creation of grid, Center divs

--- scripts.js
+++ scripts.js
@@ -1,32 +1,30 @@
 const drawingPad = document.querySelector('.container');
 const gridChanger = document.querySelector('.grid')
 
-function clear(){
+function redraw(gridNumber){
     while(drawingPad.firstChild){
         drawingPad.removeChild(drawingPad.firstChild);
     }
-}
 
-function resize(gridNumber){
     if(!Number(gridNumber)) return;
     let number = Number(gridNumber);
     let pxSize = 400/number - 2;
-    
+
     for(let i = 0; i < number*number; i++){
         const gridBox = document.createElement('div');
         gridBox.style.width = `${pxSize}px`;
         gridBox.style.height = `${pxSize}px`;
         gridBox.style.border = "1px solid grey";
-
-        gridBox.addEventListener('mouseover', () => {
-            gridBox.classList.add("colored");
-        });
-
         drawingPad.appendChild(gridBox);
     }
 }
 
 gridChanger.addEventListener('click', () => {
-    clear();
-    resize(prompt("Number of Squares per side? e.g 16 in a 16x16"));
+    redraw(prompt("Number of Squares per side? e.g 16 in a 16x16"));
+    
+    drawingPad.childNodes.forEach(box => {
+        box.addEventListener('mouseover', () => {
+            box.classList.add("colored")
+        });
+    });
 });
